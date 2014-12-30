31cc5fbf011862ccabe6b6b3014f4f0c52e5d61f
Jean-Luc Petit <jpetit@datawords.com>
2014-12-30 17:24:21 +0100

    Implement constructor.

--- Bomifier.php
+++ Bomifier.php
@@ -14,6 +14,15 @@ class Bomifier
     protected $uri;
 
     /**
+     * The uri is null to assume backward compatibility.
+     * @param string $uri
+     */
+    protected function __construct($uri = null)
+    {
+        $this->setUri($uri);
+    }
+
+    /**
      * Return the BOM if the file if it exists.
      * @return int|string
      */
@@ -58,21 +67,21 @@ class Bomifier
     public function getBom($encoding = 'UTF-8')
     {
         $boms = array(
-            'UTF-8' => pack('CCC', 0xef, 0xbb, 0xbf),
-            'UTF-16 Big Endian' => pack('CC', 0xfe, 0xff),
+            'UTF-8'                => pack('CCC', 0xef, 0xbb, 0xbf),
+            'UTF-16 Big Endian'    => pack('CC', 0xfe, 0xff),
             'UTF-16 Little Endian' => pack('CC', 0xff, 0xfe),
-            'UTF-32 Big Endian' => pack('CCCC', 0x00, 0x00, 0xfe, 0xff),
+            'UTF-32 Big Endian'    => pack('CCCC', 0x00, 0x00, 0xfe, 0xff),
             'UTF-32 Little Endian' => pack('CCCC', 0xff, 0xfe, 0x00, 0x00),
-            'SCSU' => pack('CCC', 0x0e, 0xfe, 0xff),
-            'UTF-7 (1)' => pack('CCCC', 0x2b, 0x2f, 0x76, 0x38),
-            'UTF-7 (2)' => pack('CCCC', 0x2b, 0x2f, 0x76, 0x39),
-            'UTF-7 (3)' => pack('CCCC', 0x2b, 0x2f, 0x76, 0x2b),
-            'UTF-7 (4)' => pack('CCCC', 0x2b, 0x2f, 0x76, 0x2f),
-            'UTF-7 (5)' => pack('CCCCC', 0x2b, 0x2f, 0x76, 0x38, 0x2d),
-            'UTF-1' => pack('CCC', 0xF7, 0x64, 0x4c),
-            'UTF-EBCDIC' => pack('CCCC', 0xdd, 0x73, 0x66, 0x73),
-            'BOCU-1' => pack('CCC', 0xfb, 0xee, 0x28),
-            'GB-18030' => pack('CCCC', 0x84, 0x31, 0x95, 0x33),
+            'SCSU'                 => pack('CCC', 0x0e, 0xfe, 0xff),
+            'UTF-7 (1)'            => pack('CCCC', 0x2b, 0x2f, 0x76, 0x38),
+            'UTF-7 (2)'            => pack('CCCC', 0x2b, 0x2f, 0x76, 0x39),
+            'UTF-7 (3)'            => pack('CCCC', 0x2b, 0x2f, 0x76, 0x2b),
+            'UTF-7 (4)'            => pack('CCCC', 0x2b, 0x2f, 0x76, 0x2f),
+            'UTF-7 (5)'            => pack('CCCCC', 0x2b, 0x2f, 0x76, 0x38, 0x2d),
+            'UTF-1'                => pack('CCC', 0xF7, 0x64, 0x4c),
+            'UTF-EBCDIC'           => pack('CCCC', 0xdd, 0x73, 0x66, 0x73),
+            'BOCU-1'               => pack('CCC', 0xfb, 0xee, 0x28),
+            'GB-18030'             => pack('CCCC', 0x84, 0x31, 0x95, 0x33),
         );
 
         if ('all' == $encoding) {
